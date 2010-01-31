c9d69c1bc28368b3dbc9c9e371902ce1b3892643
Anderson Mills <nodog@konfuzo.net>
2010-01-31 22:39:57 +0100

    bugfix in Noise, Damp with attack, better DefaultInstrument.

--- src/ddf/minim/ugens/DefaultInstrument.java
+++ src/ddf/minim/ugens/DefaultInstrument.java
@@ -1,30 +1,51 @@
 package ddf.minim.ugens;
 import ddf.minim.AudioOutput;
+//import ddf.minim.effects.IIRFilter;
+import ddf.minim.effects.LowPassSP;
 
 public class DefaultInstrument implements Instrument
 {
-	Oscil sineOsc, lFOOsc;
-	Gain  gainGate;
+	Oscil toneOsc;
+	Noise noiseGen;
+	Damp noiseEnv, toneEnv;
+	//Gain toneEnv;
+	//Damp toneEnv;
 	AudioOutput output;
+	Bus summer;
+	LowPassSP lpFilter;
 		  
 	public DefaultInstrument( float frequency, AudioOutput output )
 	{
 		this.output = output;
 		    
 		float amplitude = 0.3f; 
-		sineOsc = new Oscil( frequency, amplitude, Waves.Triangle );
-		gainGate = new Gain( 0.0f );
-		sineOsc.patch( gainGate );
+		noiseGen = new Noise( 0.4f*amplitude, Noise.Tint.WHITE );
+		noiseEnv = new Damp( 0.05f );
+		lpFilter = new LowPassSP( 2.0f*frequency, output.sampleRate() );
+		toneOsc = new Oscil( frequency, 0.9f*amplitude, Waves.Triangle );
+		//toneEnv = new Damp( 1.0f );
+		toneEnv = new Damp( 2.0f/frequency, 1.0f );
+		//toneEnv = new Gain( 0f );
+		summer = new Bus();
+		
+		toneOsc.patch( toneEnv ).patch( summer );
+		noiseGen.patch( noiseEnv ).patch( lpFilter).patch( summer );
+		//.patch( output );
 	}
 		  
 	public void noteOn( float dur )
 	{
-		gainGate.setValue( 1.0f );
-		gainGate.patch( output );
+		summer.patch( output );
+		toneEnv.setDampTimeFromDuration( dur );
+		toneEnv.noteOn();
+		noiseEnv.noteOn();
+		//toneEnv.setValue( 1.0f );
+		//summer.patch( output );
 	}
 		  
 	public void noteOff()
 	{
-		gainGate.setValue( 0.0f );
+		//toneEnv.setValue( 0.0f );
+		summer.unpatch( output );
 	}
 }
