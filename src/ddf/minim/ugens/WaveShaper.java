f67e624e5c8fdf3f47b9d57b3c2631f2664213c3
Anderson Mills <nodog@konfuzo.net>
2010-04-08 17:52:43 +0200

    Fixed WaveShaper + JavaDocs, removed NicolasDelay.

--- src/ddf/minim/ugens/WaveShaper.java
+++ src/ddf/minim/ugens/WaveShaper.java
@@ -1,79 +1,126 @@
 package ddf.minim.ugens;
 
+import ddf.minim.Minim;
 
+/**
+ * A UGen which provides waveshaping distortion.  The incoming "audio" signal
+ * is used as an index to a Wavetable containing a "mapping" function and the
+ * output of the waveshaper is the value in the Wavetable given by the index.
+ * The incoming wave is expected to have values between -1 and 1 although exceeding
+ * this range can be used expressively.  The input signal is then
+ * normalized so that -1 to 1 becomes 0 and 1 to provide the index value.  The output 
+ * waveshape is then multiplied by an output amplitude. 
+ * 
+ * A library of shapes is defined, that the user can call.
+ * The shapes are Wavetables, which can be used in a creative way 
+ * (using waveforms from the Waves library for example).
+ * 
+ * @author Nicolas Brix, Anderson Mills
+ */
 public class WaveShaper extends UGen 
 {
 	/**
-	 * A distortion UGen.
-	 * 
-	 * A library of shapes is defined, that the user can call.
-	 * The shapes are Wavetables, which can be used in a creative way 
-	 * (using waveforms from the Waves library for example)
-	 * 
-	 * @author nb
+	 * The default input is "audio."
 	 */
-	
-	//audio input
 	public UGenInput audio;
+	/**
+	 * The output amplitude
+	 */
+	public UGenInput outAmplitude;
+	/**
+	 * The mapping amplitude of the input signal
+	 */
+	public UGenInput mapAmplitude;
 	
+	// the current mapping amplitude of the input signal
+	float mapAmp;
+	// the current output amplitude of the input signal
+	float outAmp;
+	// flag to wrap the map around the ends instead of hitting the edge
+	boolean wrapMap;
+	// the current waveshape for mapping
+	Wavetable mapShape;
 	
-	float amount2;
-	public UGenInput amount;
-	Wavetable shape;
-	
-	public WaveShaper(Wavetable sh, float am)
+	/**
+	 * Constructor for WaveShaper.
+	 * mapWrap, a boolean flag to wrap the map around the ends 
+	 * instead of hitting the edge, defaults to false.
+	 * @param mapAmp
+	 * 			amplitude over which to map the incoming signal
+	 * @param mapShape
+	 *			waveshape over which to map the incoming signal
+	 */
+	public WaveShaper( float outAmp, float mapAmp, Wavetable mapShape )
+	{
+		this( outAmp, mapAmp, mapShape, false );
+	}
+	/**
+	 * Constructor for WaveShaper.
+	 * @param mapAmp
+	 * 			amplitude over which to map the incoming signal
+	 * @param mapShape
+	 *			waveshape over which to map the incoming signal
+	 * @param wrapMap
+	 * 			boolean flag to wrap the map instead of hit the edge and stick
+	 */
+	public WaveShaper( float outAmp, float mapAmp, Wavetable mapShape, boolean wrapMap )
 	{
 		super();
-		shape=sh;
-		amount2=am;
 		audio = new UGenInput(InputType.AUDIO);
-		amount = new UGenInput(InputType.CONTROL);
+		mapAmplitude = new UGenInput(InputType.CONTROL);
+		outAmplitude = new UGenInput(InputType.CONTROL);
+		this.mapShape = mapShape;
+		this.outAmp = outAmp;
+		this.mapAmp = mapAmp;
+		this.wrapMap = wrapMap;
 	}
-	
-	
 
-	
-	
-	
-	
 	//the input signal is supposed to be less than 1 in amplitude 
 	//as Wavetable is basically an array of floats accessed via a 0 to 1.0 index, 
-	//some shifting+scaling has to be done
-	
+	//some shifting+scaling has to be done	
 	//the shape is supposed to be -1 at [0] and +1 at [length].
-	
+	@Override
 	protected void uGenerate(float[] channels)
 	{
-		if ((amount != null) && (amount.isPatched()))
+		// get the incoming mapAmplitude if it exists
+		if ((mapAmplitude != null) && (mapAmplitude.isPatched()))
 		{
-			amount2 = amount.getLastValues()[0];
-
+			mapAmp = mapAmplitude.getLastValues()[0];
 		}
-		
-		
+		// get the incoming outAmplitude if it exists
+		if ((outAmplitude != null) && (outAmplitude.isPatched()))
+		{
+			outAmp = outAmplitude.getLastValues()[0];
+		}
+
+		// run over the length of the channel array
 		for(int i = 0; i < channels.length; i++)
 		{
+			// bring in the audio as index, scale by the map amplitude, and normalize
+			float tmpIndex = ( mapAmp*audio.getLastValues()[i] )/2.0f + 0.5f;
 			
-			float a= amount2*audio.getLastValues()[i];
+			// handle the cases where it goes out of bouds
+			if ( wrapMap )  // wrap oround
+			{
+				// what's left after dividing by 1?
+				tmpIndex %= 1.0f;
+				// I don't like that remaider gives the same sign as the first argument
+				if ( tmpIndex < 0.0f )
+				{
+					tmpIndex += 1.0f;
+				}
+			}
+			else if ( tmpIndex > 1.0f )  // otherwise cap at 1
+			{
+				tmpIndex = 1.0f;
+			} 
+			else if ( tmpIndex < 0.0f ) // and cap on the bottom at 0
+	        {
+	        	tmpIndex = 0.0f;
+	        }
 			
-			channels[i]= (Math.abs(a)> 1f)? Math.signum(a) : shape.value(a/2+0.5f);
-			channels[i]/=amount2;
+			// now that tmpIndex is good, look up the wavetable value and multiply by outAmp
+			channels[i] = outAmp*mapShape.value( tmpIndex );
 		}
-	}
-
-	//public final static Wavetable Diode = new Wavetable(new float[] {-1,-1,1,1});
-	public final static Wavetable Diode = WavetableGenerator.gen7(512, new float[] {-1,-1,1,1}, new int[]{128,256,128});
-	
-	//this one is a 
-	public final static Wavetable Square = WavetableGenerator.gen7(512, new float[] {-1,-1,1,1}, new int[]{256,0,256});
-	//public final static Wavetable Saw = new Wavetable(new float[] {-1,1});
-	
-	public static final Wavetable TruncSine()
-	{
-		Wavetable sine = WavetableGenerator.gen10(512, new float [] {1});
-		Wavetable truncsine = new Wavetable(new float[256]);
-		System.arraycopy(sine.getWaveform(), 128,truncsine.getWaveform(), 0, 256);
-		truncsine.invert();
-		return truncsine;
 	}
 }
