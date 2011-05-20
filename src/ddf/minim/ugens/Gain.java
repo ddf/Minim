c2aaf14234af251a151d553cb9c834aa6e49a8ec
J Anderson Mills III <nodog@konfuzo.net>
2011-05-20 17:10:43 -0500

    Fixing a small bug in the Gain UGen which caused no effect for setValue().

--- src/ddf/minim/ugens/Gain.java
+++ src/ddf/minim/ugens/Gain.java
@@ -68,7 +68,10 @@ public class Gain extends UGen
 	protected void uGenerate(float[] channels) 
 	{
 		// TODO: not fond of the fact that we cast up to doubles for this math function.
-		mValue = (float)Math.pow(10.0, (0.05 * gain.getLastValue()));
+		if ( gain.isPatched() )
+		{
+			mValue = (float)Math.pow(10.0, (0.05 * gain.getLastValue()));
+		}	
 		
 		for(int i = 0; i < channels.length; ++i)
 		{
