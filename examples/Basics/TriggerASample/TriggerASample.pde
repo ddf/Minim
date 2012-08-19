3ce1ad322ea8a533c6e0daf4ba31efc2257fbdf2
Damien Di Fede <dcomposer@gmail.com>
2012-08-19 18:45:13 -0500

    focus on documentation for createSample and loadSample in Minim with some improvements to AudioSamples doc to match.

--- examples/Basics/TriggerASample/TriggerASample.pde
+++ examples/Basics/TriggerASample/TriggerASample.pde
@@ -1,14 +1,16 @@
 /**
-  * This sketch demonstrates how to use the <code>loadSample</code> method of <code>Minim</code>. The <code>loadSample</code> 
-  * method allows you to specify the sample you want to load with a <code>String</code> and optionally specify what you 
-  * want the buffer size of the returned <code>AudioSample</code> to be. Minim is able to load wav files, au files, aif
-  * files, snd files, and mp3 files. When you call <code>loadSample</code>, if you just specify the filename it will try 
-  * to load the sample from the data folder of your sketch. However, you can also specify an absolute path (such as 
-  * "C:\foo\bar\thing.wav") and the file will be loaded from that location (keep in mind that won't work from an applet). 
-  * You can also specify a URL (such as "http://www.mysite.com/mp3/song.mp3") but keep in mind that if you run the sketch 
-  * as an applet you may run in to security restrictions if the applet is not on the same domain as the file you want to load. 
-  * You can get around the restriction by signing all of the jars in the applet. Before you exit your sketch make sure you call the 
-  * <code>close</code> method of any <code>AudioSamples</code>'s you have received from <code>loadSample</code>.
+  * This sketch demonstrates how to use the <code>loadSample</code> method of <code>Minim</code>. 
+  * The <code>loadSample</code> method allows you to specify the sample you want to load 
+  * with a <code>String</code> and optionally specify what you want the buffer size of the 
+  * returned <code>AudioSample</code> to be. Minim is able to load wav files, au files, aif
+  * files, snd files, and mp3 files. When you call <code>loadSample</code>, if you just 
+  * specify the filename it will try to load the sample from the data folder of your sketch. 
+  * However, you can also specify an absolute path (such as "C:\foo\bar\thing.wav") and the 
+  * file will be loaded from that location (keep in mind that won't work from an applet). 
+  * You can also specify a URL (such as "http://www.mysite.com/mp3/song.mp3") but keep in mind 
+  * that if you run the sketch as an applet you may run in to security restrictions 
+  * if the applet is not on the same domain as the file you want to load. You can get around 
+  * the restriction by signing all of the jars in the applet.
   * <p>
   * An <code>AudioSample</code> is a special kind of file playback that allows
   * you to repeatedly <i>trigger</i> an audio file. It does this by keeping the
@@ -18,8 +20,8 @@
   * for long sounds (like entire songs, for example) because the entire file is
   * kept in memory.
   * <p>
-  * Use 'k' and 's' to trigger a kick drum sample and a snare sample, respectively. You will see their waveforms
-  * drawn when they are played back.
+  * Use 'k' and 's' to trigger a kick drum sample and a snare sample, respectively. 
+  * You will see their waveforms drawn when they are played back.
   */
 
 import ddf.minim.*;
--- src/ddf/minim/AudioSample.java
+++ src/ddf/minim/AudioSample.java
@@ -21,16 +21,21 @@ package ddf.minim;
 import ddf.minim.spi.AudioOut;
 
 /**
- * An <code>AudioSample</code> is a special kind of file playback that allows
- * you to repeatedly <i>trigger</i> an audio file. It does this by keeping the
- * entire file in an internal buffer and then keeping a list of trigger points.
- * It is {@link Recordable} and {@link Effectable} so access to the samples is
- * available and <code>AudioEffect</code>s can be attached to it, but there are
- * not the cueing abilities found on an <code>AudioSnippet</code> and
- * <code>AudioPlayer</code>. All you can do is <code>trigger()</code> the sound.
- * However, you can trigger the sound even if it is still playing back. It is
- * not advised that you use this class for long sounds (like entire songs, for
- * example) because the entire file is kept in memory.
+ * An <code>AudioSample</code> keeps the entire file in an internal buffer and
+ * all you can do is <code>trigger()</code> the sound. However, you can trigger
+ * the sound even if it is still playing back. It is not advised that you use
+ * this class for long sounds (like entire songs, for example) because the
+ * entire file is kept in memory.
+ * <p>
+ * To create an AudioSample you must use either the loadSample or createSample 
+ * methods of the Minim class.
+ * <p>
+ * AudioSample also provides most of the same methods as AudioPlayer for
+ * controlling volume, panning, and so forth.
+ * 
+ * @example Basics/TriggerASample
+ * 
+ * @related Minim
  * 
  * @author Damien Di Fede
  * 
@@ -44,7 +49,10 @@ public abstract class AudioSample extends AudioSource
 	static public final int	RIGHT	= 2;
 
 	/**
-	 * Constructs an AudioSample object that gets its audio from stream.
+	 * @invisible
+	 * 
+	 *            Constructs an AudioSample object that gets its audio from
+	 *            stream.
 	 * 
 	 * @param stream
 	 *            the stream that will provide the audio
@@ -55,24 +63,31 @@ public abstract class AudioSample extends AudioSource
 	}
 
 	/**
+	 * Get the AudioMetaData for this sample. This will mostly be useful if you
+	 * have created an AudioSample from an mp3 file and want to get at some of
+	 * the most common ID3 tags.
 	 * 
 	 * @return the AudioMetaData for the sample.
+	 * 
+	 * @related AudioMetaData
 	 */
 	public abstract AudioMetaData getMetaData();
 
 	/**
-	 * Gets the samples for the requested channel number as a float array.
+	 * Gets the samples for the requested channel number as a float array. Use
+	 * either AudioSample.LEFT or AudioSample.RIGHT.
 	 * 
 	 * @param channelNumber
 	 *            the channel you want the samples for
-	 * @return the samples in a float array
+	 * 
+	 * @return float[]: the samples in the specified channel
 	 */
 	public abstract float[] getChannel(int channelNumber);
 
 	/**
-	 * Gets the length in milliseconds of the buffered audio.
+	 * Gets the length in milliseconds of this AudioSample.
 	 * 
-	 * @return the length in millisecons
+	 * @return int: the length in milliseconds
 	 */
 	public abstract int length();
 
@@ -84,7 +99,7 @@ public abstract class AudioSample extends AudioSource
 	public abstract void trigger();
 
 	/**
-	 * Stops all sound being produced.
+	 * Stops all sound being produced by this AudioSample.
 	 */
 	public abstract void stop();
 }
