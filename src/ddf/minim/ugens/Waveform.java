abfbeee576c10d711cea8f0e8a526b9812934461
Damien Di Fede <dcomposer@gmail.com>
2012-11-30 14:15:31 +0100

    finished Wavetable and Waveform docs and created an example to demonstrate most methods of Wavetable.

--- src/ddf/minim/ugens/Waveform.java
+++ src/ddf/minim/ugens/Waveform.java
@@ -5,10 +5,32 @@ package ddf.minim.ugens;
  * between 0 and 1. 
  * 
  * @author Damien Di Fede
+ * 
+ * @related Oscil
+ * @related Wavetable
  *
  */
 
 public interface Waveform 
 {
+	/**
+	 * Sample the Waveform at the location specified. 
+	 * As an example, if the Waveform represents a sine wave,
+	 * then we would expect the following:
+	 * <p>
+	 * <pre>
+	 * waveform.value( 0.25f ) == sin( PI/2 )
+	 * waveform.value( 0.5f ) == sin( PI )
+	 * waveform.value( 0.75f ) == sin( 3*PI/2 )
+	 * </pre> 
+	 * 
+	 * @shortdesc Sample the Waveform at the location specified.
+	 * 
+	 * @param at
+	 * 			float: a value in the range [0,1]
+	 * @return float: the value of the Waveform at the sampled location
+	 * 
+	 * @related Waveform
+	 */
 	float value(float at);
 }
